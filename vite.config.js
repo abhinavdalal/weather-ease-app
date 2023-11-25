836d7a899c9f0284ace760bb5b9b205012aecaf2
Akanksha <apekshaj702@gmail.com>
2023-11-25 13:14:57 +0530

    removed swc

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// // export default defineConfig({
-// //   plugins: [react()],
-// // })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
 // export default ({ mode }) => {
 //   return defineConfig({
 //     plugins: [
@@ -12,9 +12,9 @@
 //     ],
 //   });
 // };
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-});
+// export default defineConfig({
+//   plugins: [react()],
+// });
