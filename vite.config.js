90986fffa7dc5452d40554aef07e9862b9570b96
Akanksha <apekshaj702@gmail.com>
2023-11-25 13:06:54 +0530

    try github pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [
-      react(),
-    ],
-  });
-};
+// // https://vitejs.dev/config/
+// // export default defineConfig({
+// //   plugins: [react()],
+// // })
+// export default ({ mode }) => {
+//   return defineConfig({
+//     plugins: [
+//       react(),
+//     ],
+//   });
+// };
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+export default defineConfig({
+  plugins: [react()],
+});
