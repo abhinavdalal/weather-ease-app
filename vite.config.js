7c6ae1b2367613d20286e76654b8b5fd1259eb89
Abhinav Dalal <abhinav@retire100.live>
2023-11-28 21:58:26 +0530

    trying with base in the vite config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-ease-app/'
 })
 // export default ({ mode }) => {
 //   return defineConfig({
