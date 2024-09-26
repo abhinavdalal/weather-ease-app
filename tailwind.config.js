d31cad32b4ce6a3c579a7cc8570a9b432f753cca
Abhinav Dalal <abhinav@retire100.live>
2024-09-26 21:47:42 +0530

    added the tailwindcss config to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-      // Adjust the path based on your project structure
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
-})
+  base: './',
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  }
+});
+
 // export default ({ mode }) => {
 //   return defineConfig({
 //     plugins: [
