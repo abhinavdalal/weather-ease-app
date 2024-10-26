27fa7a596879a5f13d1d54f50e78ae8ed9c32f5f
Akanksha <apekshaj702@gmail.com>
2024-10-26 10:28:07 +0530

    added hindi english split components in helpline

--- src/pages/Helpline/Helpline.jsx
+++ src/pages/Helpline/Helpline.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Helpline.css';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+// import List from '@mui/material/List';
+// import ListItem from '@mui/material/ListItem';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 import SimpleBottomNavigation from "../../components/BottomNavigationTest";
 import ResponsiveAppBar from "../../components/OpenBar";
+import EnglishList from './components/EnglishList';
+import HindiList from './components/HindiList';
 
 const Helpline = () => {
+  const [currentLanguage, setCurrentLanguage] = useState('English');
+  const handleChange = (event) => {
+    setCurrentLanguage(event.target.value);
+  };
+
   return (
     <Container>
       <ResponsiveAppBar />
@@ -15,8 +24,23 @@ const Helpline = () => {
         <Typography variant="h4" align="center" sx={{ mt: 4, mb: 2 }}>
           Emergency Helpline Numbers
         </Typography>
-       
-        <List sx={{ textAlign: 'center' }}>
+
+        <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={currentLanguage}
+    label="Language"
+    onChange={handleChange}
+  >
+    <MenuItem value={"English"}>English</MenuItem>
+    <MenuItem value={'Hindi'}>Hindi</MenuItem>
+  </Select>
+
+        <p>Current Language: {currentLanguage}</p>
+
+        { currentLanguage == "English" ? <EnglishList /> : null } 
+        { currentLanguage == "Hindi" ? <HindiList /> : null }
+        {/* <List sx={{ textAlign: 'center' }}>
           <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case of Fire (आग लगने की स्थिति में): 101</ListItem>
           <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case of you need Police (यदि आपको पुलिस की आवश्यकता है): 100</ListItem>
           <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>If you need Ambulance (अगर आपको एम्बुलेंस की जरूरत है): 102</ListItem>
@@ -32,7 +56,7 @@ const Helpline = () => {
           <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>Fumigation Helpline number (धूमन हेल्पलाइन नंबर): 02223011958</ListItem>
           <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>To track your location (अपना स्थान ट्रैक करने के लिए): 1090</ListItem>
           <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>Emergency operation centre [Mumbai] (आपातकालीन संचालन केंद्र): 1916, 02222694725</ListItem>
-        </List>
+        </List> */}
         <SimpleBottomNavigation />
       </div>
     </Container>
---
+++ src/pages/Helpline/components/HindiList.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+
+function EnglishList() {
+    return (
+        <List sx={{ textAlign: 'center' }}>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>आग लगने की स्थिति में : 101</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>यदि आपको पुलिस की आवश्यकता है : 100</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>अगर आपको एम्बुलेंस की जरूरत है : 102</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case of a disaster (किसी आपदा की स्थिति में): 1916</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>24×7 Helpline no. : 108</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>For Ambulance (एम्बुलेंस): 102</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>For Child Helpline (चाइल्ड हेल्पलाइन के लिए) : 1098</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case you need Police (यदि आपको पुलिस की आवश्यकता है): 100</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>If you are stuck in Traffic (अगर आप ट्रैफिक में फंसे हैं): 1073</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case of an accident (किसी दुर्घटना की स्थिति में): 108</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case of emergency (आपात्कालीन स्थिति में): 112</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>In case of a fire (आग लगने की स्थिति में): 101</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>Fumigation Helpline number (धूमन हेल्पलाइन नंबर): 02223011958</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>To track your location (अपना स्थान ट्रैक करने के लिए): 1090</ListItem>
+            <ListItem sx={{ fontSize: '1.2rem', textAlign: 'center' }}>Emergency operation centre [Mumbai] (आपातकालीन संचालन केंद्र): 1916, 02222694725</ListItem>
+        </List>
+    )
+}
+
+export default EnglishList
