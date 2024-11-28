086e8a9cc631515807b80d11d251f2204b0c1d83
Aradhya Kadam <arakad310310@akanksha.org>
2024-11-28 08:56:20 +0530

    changed color of the modal

--- src/pages/awareness/awareness.jsx
+++ src/pages/awareness/awareness.jsx
@@ -26,12 +26,13 @@ const style = {
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: 400,
-    bgcolor: 'background.paper',
+    bgcolor: 'blue',
     border: 'none',
     borderRadius: '10px',
     boxShadow: 24,
-    p: 4,
+    p: 5,
     textAlign: 'center',
+    color: 'white',
 };
 
 const descriptions = {
